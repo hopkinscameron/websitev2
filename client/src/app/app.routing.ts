e0e2746590719d9df59394afbabd6e4fe8f62083
Cameron Hopkins <hopkinsc92@gmail.com>
2020-01-09 12:33:13 -0600

    ready for developing

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const appRoutes: Routes = [
-    { path: 'crisis-center', component: CrisisListComponent },
-    { path: 'hero/:id', component: HeroDetailComponent },
-    {
-        path: 'heroes',
-        component: HeroListComponent,
-        data: { title: 'Heroes List' }
-    },
-    { path: '', redirectTo: '/heroes', pathMatch: 'full'},
-    { path: '**', component: PageNotFoundComponent }
+    // { path: 'crisis-center', component: CrisisListComponent },
+    // { path: 'hero/:id', component: HeroDetailComponent },
+    // {
+    //     path: 'heroes',
+    //     component: HeroListComponent,
+    //     data: { title: 'Heroes List' }
+    // },
+    // { path: '', redirectTo: '/heroes', pathMatch: 'full'},
+    // { path: '**', component: PageNotFoundComponent }
 ];
